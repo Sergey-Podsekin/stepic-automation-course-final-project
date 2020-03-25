9079fabde64454051ddafbf891ae1ea4264ae98b
Sergey Podsekin <sergeykenzo@gmail.com>
2020-03-25 21:08:53 +0300

    Add login page and test_login_page

---
+++ test_login_page.py
@@ -0,0 +1,22 @@
+from pages.login_page import LoginPage
+
+
+def test_check_url(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_check_login_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_check_register_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
