25f4fe553ed73d59ea291b64d4347f1477d0b8c5
Sergey Podsekin <sergeykenzo@gmail.com>
2020-04-12 22:36:54 +0300

    Add product page and test_product_page, add test method check price, check success message

---
+++ test_product_page.py
@@ -0,0 +1,50 @@
+from pages.product_page import ProductPage
+
+'''
+def test_add_to_basket_button_is_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+
+
+def test_price_is_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_price()
+
+
+def test_same_price_in_title_and_product_tab(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.same_price_in_title_and_product_tab()
+
+
+def test_same_titles_in_breadcrumb_and_main(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.same_price_in_title_and_product_tab()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.foo()
+
+'''
+def test_should_be_message_added_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_message_added_to_basket()
+
+
+def test_book_price_should_be_added_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.book_price_should_be_added_to_basket()
