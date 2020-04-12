9fe14e5d0d04b7b6669d53f9cbba6366cb674a8d
Sergey Podsekin <sergeykenzo@gmail.com>
2020-04-12 22:39:28 +0300

    Add base page method, locators product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
+    PRICE_COLOR = (By.CSS_SELECTOR, ".col-sm-6 .price_color")
+    PRICE_IN_PRODUCT_INFORMATION = (By.CSS_SELECTOR, ".table tr:nth-child(3) td")
+    BOOK_TITLE_IN_BREADCRUMB = (By.CSS_SELECTOR, ".breadcrumb .active")
+    BOOK_TITLE_MAIN = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alert-info strong")
+    MESSAGE_BOOK_HAS_BEEN_ADDED = (By.CSS_SELECTOR, "#messages>.alert:nth-child(1) strong")
